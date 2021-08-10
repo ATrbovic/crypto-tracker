6c08a12bea321f1bdac0a50851d27dbd5a4ccb0d
Alex Trbovic <alextrbovic@Alexs-MacBook-Pro.local>
2021-08-10 10:28:17 -0400

    added search functionality

---
+++ client/src/services/searchCryptos.js
@@ -0,0 +1,17 @@
+const searchCryptos = async (searchQuery) => {
+  try {
+    const response = await fetch (`/api/v1/search/${searchQuery}`)
+    if(!response.ok){
+      const errorMessage = `${response.status}: (${response.statusText})`
+      const error = new Error(errorMessage)
+      throw(error)
+    } else {
+      const searchedCryptosData = await response.json()
+      return searchedCryptosData.cryptos
+    }
+  } catch(err) {
+    console.log(`Error in Fetch: ${err.message}`)
+  }
+}
+
+export default searchShows
---
+++ server/src/routes/api/v1/searchRouter.js
@@ -0,0 +1,37 @@
+import express from "express"
+import { Crypto } from "../../../models/Crypto.js"
+import CryptoSerializer from "../../../serializers/CryptoSerializer.js"
+
+const searchRouter = new express.Router()
+
+searchRouter.get("/", async (req, res) => {
+  try {
+    const cryptos = await Crypto.query()
+    const serializedCryptos = await Promise.all(
+      cryptos.map(async (crypto) => {
+        return await CryptoSerializer.getSummary(crypto)
+      })
+    )
+    return res.status(200).json({ shows: serializedCryptos })
+  } catch(error) {
+    return res.status(500).json({ error })
+  }
+})
+
+searchRouter.get("/:searchQuery", async (req, res) => {
+  const searchQuery = req.params.searchQuery
+  try {
+    const cryptos = await Crypto.query()
+    const searchedCryptos = cryptos.filter(crypto.getRandomValues.toLowerCase().includes(searchQuery.toLowerCase()))
+    const serializedCryptos = await Promise.all(
+      searchedCryptos.map(async (show) => {
+        return await CryptoSerializer.getSummary(crypto)
+      })
+    )
+    return res.status(200).json({ cryptos: serializedCryptos })
+  } catch(error) {
+    return res.status(500).json({ error })
+  }
+})
+
+export default searchRouter
